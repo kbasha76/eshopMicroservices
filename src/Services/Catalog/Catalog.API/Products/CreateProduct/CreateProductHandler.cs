813cf726baf2f15c38bbe118829a30f51b50d8d5
kbasha76 <skb786in@hotmail.com>
2024-12-26 01:18:12 +0530

    Initial Commit - CreateProduct

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description,
+        string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler :
+        ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command,
+            CancellationToken cancellationToken)
+        {
+            // Business Logic to create a product
+            // 1. create a product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            // 2. save to database
+
+            // 3. return CreateProductResult result
+            //throw new NotImplementedException();
+            var productId = Guid.NewGuid();
+            return Task.FromResult<CreateProductResult>(new CreateProductResult(productId));
+        }
+    }
+}
